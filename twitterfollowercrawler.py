9f660c7a0d501225cd48378e6760d374f78cfc44
K Harishankar <48408097+Haritronic909@users.noreply.github.com>
2020-03-25 15:24:12 +0530

    Update twitterfollowercrawler.py

--- twitterfollowercrawler.py
+++ twitterfollowercrawler.py
@@ -1,12 +1,14 @@
 import requests
 from bs4 import BeautifulSoup as bs
-x = 'Facebook'#
+
+x = '<input the required account name wherever case sensitive>'
 URL = 'https://twitter.com/'+x
 page = requests.get(URL)
 inputTag = soup.find(href="/"+x+"/followers")
 output = inputTag['title']
+
 print(output)#To get uformatted output
 
-#
+#uncomment the lines below to obtain only numerical output
 #followers=''.join(i for i in output if i.isdigit())
 #print(followers)
